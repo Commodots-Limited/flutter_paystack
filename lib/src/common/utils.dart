1b48022e8bb26f35a3edc6ad2150695e0d8d16b3
Mogbonjubade Ayoade <ma@commodots.com>
2023-08-02 14:43:06 +0100

    Update utils.dart
    Use actually currency value

--- lib/src/common/utils.dart
+++ lib/src/common/utils.dart
@@ -18,7 +18,8 @@ class Utils {
 
   static String formatAmount(num amountInBase) {
     if (_currencyFormatter == null) throw "Currency formatter not initialized.";
-    return _currencyFormatter!.format((amountInBase / 150));
+    #return _currencyFormatter!.format((amountInBase / 150));
+    return _currencyFormatter!.format((amountInBase));
   }
 
   /// Add double spaces after every 4th character
